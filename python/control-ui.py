dad235360c1dd27d6489aade065c58f67e1723c8
Grey Christoforo <grey@christoforo.net>
2020-02-20 19:13:21 +0000

    wip on refactor

--- python/control-ui.py
+++ python/control-ui.py
@@ -178,3 +178,54 @@ lg.debug("Logging started.")
 window = builder.get_object("mainWindow")
 window.show_all()
 Gtk.main()
+
+
+
+class Application(Gtk.Application):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, application_id="net.christoforo.control-ui",
+                         flags=Gio.ApplicationFlags.HANDLES_COMMAND_LINE,
+                         **kwargs)
+        self.window = None
+
+        self.add_main_option("test", ord("t"), GLib.OptionFlags.NONE,
+                             GLib.OptionArg.NONE, "Command line test", None)
+
+    def do_startup(self):
+        Gtk.Application.do_startup(self)
+
+        self.builder = Gtk.Builder()
+        self.builder.add_from_file("ui.glade")
+
+    def do_activate(self):
+        # We only allow a single window and raise any existing ones
+        if self.window is None:
+            # Windows are associated with the application
+            # when the last one is closed the application shuts down
+            self.window = AppWindow(application=self, title="Main Window")
+
+        self.window.present()
+
+    def do_command_line(self, command_line):
+        options = command_line.get_options_dict()
+        # convert GVariantDict -> GVariant -> dict
+        options = options.end().unpack()
+
+        if "test" in options:
+            # This is printed on the main instance
+            print("Test argument recieved: %s" % options["test"])
+
+        self.activate()
+        return 0
+
+    def on_about(self, action, param):
+        about_dialog = Gtk.AboutDialog(transient_for=self.window, modal=True)
+        about_dialog.present()
+
+    def on_quit(self, action, param):
+        self.quit()
+
+if __name__ == "__main__":
+    app = Application()
+    app.run(sys.argv)
--- run_local.sh
+++ run_local.sh
@@ -3,4 +3,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "${DIR}/python"
 
-python control-ui.py
+GTK_THEME=Adwaita:light python control-ui.py
