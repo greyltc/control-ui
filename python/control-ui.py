d133dcba341a572e2938333e0ce625a526506831
Grey Christoforo <grey@christoforo.net>
2020-02-22 02:14:22 +0000

    refactored

--- python/control-ui.py
+++ python/control-ui.py
@@ -7,13 +7,14 @@ import logging
 import systemd.journal
 #import logging.handlers
 import gi
-import os
+import sys
+#import os
 #os.environ["DEBUSSY"] = "1"
 
 #gi.require_version('WebKit2', '4.0')
 gi.require_version('Gtk', '3.0')
 gi.require_version('Gdk', '3.0')
-from gi.repository import Gtk, Gdk
+from gi.repository import GLib, Gio, Gtk
 #Gdk.set_allowed_backends('broadway')
 #from gi.repository.WebKit2 import WebView, Settings
 #gi.require_version('WebKit2', '4.0')
@@ -34,60 +35,15 @@ ch.setFormatter(logFormat)
 lg.addHandler(ch)
 lg.addHandler(sysL)
 
-#Webkit.WebView()
-
-
-
-
-
-builder = Gtk.Builder()
-builder.add_from_file("ui.glade")
-
-class Handler:
-    def on_mainWindow_destroy(self, *args):
-        lg.debug("Destroying")
-        Gtk.main_quit()
-
-    def on_runCodeButton_clicked(self, button):
-        lg.debug("Hello World!")
-
-    def on_iv_devs_icon_release(self, icon, a, b):
-        lg.debug("devs icon released")
-        dd = builder.get_object("devDialog")
-        dd.show_all()
-
-
-builder.connect_signals(Handler())
-logTB = builder.get_object("tbLog")
-ltv = builder.get_object("ltv")
-tv = builder.get_object("tv")
-devTV = builder.get_object("devTV")
-
-numSubstrates = 20
-numPix = 6
-
-# make the store
-store = Gtk.TreeStore(str, bool, str, bool, str, int)
-for i in range(numSubstrates):
-    # the iter piter is returned when appending the author
-    store.append(None, [f"{i+1}", False, "", True, "Missing substrate label", 5])
-
-
-tv.set_model(store)
-# the cellrenderer for the column - text
-renderText = Gtk.CellRendererText()
-# the column is created
-numbers = Gtk.TreeViewColumn("Substrate", renderText, text=0, editable=1)
-# and it is appended to the treeview
-tv.append_column(numbers)
-labels = Gtk.TreeViewColumn("Label", renderText, text=2, editable=3, placeholder_text=4, ypad=5)
-tv.append_column(labels)
-
-
 books = [["Tolstoy, Leo", ["War and Peace", True], ["Anna Karenina", False]],
          ["Shakespeare, William", ["Hamlet", False],
              ["Macbeth", True], ["Othello", False]],
          ["Tolkien, J.R.R.", ["The Lord of the Rings", False]]]
+
+#Webkit.WebView()
+
+s = 20
+d = 6
 
 # the data are stored in the model
 # create a treestore with two columns
@@ -103,19 +59,28 @@ for i in range(len(books)):
     while j < len(books[i]):
         devStore.append(piter, books[i][j])
         j += 1
-devTV.set_model(devStore)
+
+
+#devTV.set_model(devStore)
+
+
 renderer_books = Gtk.CellRendererText()
 # the first column is created
 column_books = Gtk.TreeViewColumn("Books", renderer_books, text=0)
 # and it is appended to the treeview
-devTV.append_column(column_books)
+
+
+#devTV.append_column(column_books)
 
 # the cellrenderer for the second column - boolean rendered as a toggle
 renderer_in_out = Gtk.CellRendererToggle()
 # the second column is created
 column_in_out = Gtk.TreeViewColumn("Out?", renderer_in_out, active=1)
 # and it is appended to the treeview
-devTV.append_column(column_in_out)
+
+
+
+#devTV.append_column(column_in_out)
 
 # callback function for the signal emitted by the cellrenderertoggle
 def on_toggled(widget, path):
@@ -159,73 +124,117 @@ def on_toggled(widget, path):
 renderer_in_out.connect("toggled", on_toggled)
 
 
-def myWrite(buf):
-    logTB.insert(logTB.get_end_iter(), str(buf))
-    ltv.scroll_to_iter(logTB.get_end_iter(), 0, False, 0, 0)
 
-
-def myFlush():
-    pass
-
-
-logTB.write = myWrite
-logTB.flush = myFlush
-uiLog = logging.StreamHandler(stream=logTB)
-uiLog.setFormatter(uiLogFormat)
-lg.addHandler(uiLog)
-lg.debug("Logging started.")
-
-window = builder.get_object("mainWindow")
-window.show_all()
-Gtk.main()
-
-
-
-class Application(Gtk.Application):
+class App(Gtk.Application):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, application_id="net.christoforo.control-ui",
                          flags=Gio.ApplicationFlags.HANDLES_COMMAND_LINE,
                          **kwargs)
-        self.window = None
+        self.main_win = None
+        self.b = None
+        self.numPix = 6
+        self.numSubstrates = 20
 
         self.add_main_option("test", ord("t"), GLib.OptionFlags.NONE,
                              GLib.OptionArg.NONE, "Command line test", None)
 
     def do_startup(self):
+        lg.debug("Starting up app")
         Gtk.Application.do_startup(self)
 
-        self.builder = Gtk.Builder()
-        self.builder.add_from_file("ui.glade")
+        self.b = Gtk.Builder()
+        self.b.add_from_file("ui.glade")
+
+        self.labelTree = self.b.get_object("labelTree")
+        self.deviceTree = self.b.get_object("devTV")
+        self.deviceTree.set_model(devStore)
+        self.deviceTree.append_column(column_books)
+        self.deviceTree.append_column(column_in_out)
+
+        self.labelStore = Gtk.TreeStore(str, bool, str, bool, str, int)
+        for i in range(self.numSubstrates):
+            # the iter piter is returned when appending the author
+            self.labelStore.append(None, [f"{i+1}", False, "", True, "Missing substrate label", 0])
+
+        self.labelTree.set_model(self.labelStore)
+        # the cellrenderer for the column - text
+        renderText = Gtk.CellRendererText()
+        # the column is created
+        numbers = Gtk.TreeViewColumn("Substrate", renderText, text=0, editable=1)
+        # and it is appended to the treeview
+        self.labelTree.append_column(numbers)
+        labels = Gtk.TreeViewColumn("Label", renderText, text=2, editable=3, placeholder_text=4, ypad=5)
+        self.labelTree.append_column(labels)
+
+
+
+        self.b.connect_signals(self)  # maps all ui callbacks to functions here
 
     def do_activate(self):
+        lg.debug("Activating app")
+
         # We only allow a single window and raise any existing ones
-        if self.window is None:
+        if self.main_win is None:
             # Windows are associated with the application
             # when the last one is closed the application shuts down
-            self.window = AppWindow(application=self, title="Main Window")
+            self.logTB = self.b.get_object("tbLog")  # log text buffer
+            self.ltv = self.b.get_object("ltv")  # log text view
 
-        self.window.present()
+            def myWrite(buf):
+                self.logTB.insert(self.logTB.get_end_iter(), str(buf))
+                self.ltv.scroll_to_iter(self.logTB.get_end_iter(), 0, False, 0, 0)
+
+            def myFlush():
+                pass
+
+            self.logTB.write = myWrite
+            self.logTB.flush = myFlush
+            uiLog = logging.StreamHandler(stream=self.logTB)
+            uiLog.set_name('ui')
+            uiLog.setFormatter(uiLogFormat)
+            lg.addHandler(uiLog)
+            lg.debug("Gui logging setup.")
+
+            self.main_win = self.b.get_object("mainWindow")
+            self.main_win.set_application(self)
+
+        self.main_win.present()
 
     def do_command_line(self, command_line):
+        lg.debug("Doing command line things")
         options = command_line.get_options_dict()
         # convert GVariantDict -> GVariant -> dict
         options = options.end().unpack()
 
         if "test" in options:
             # This is printed on the main instance
-            print("Test argument recieved: %s" % options["test"])
+            lg.debug('Test argument recieved: {options["test"]}')
 
         self.activate()
         return 0
 
     def on_about(self, action, param):
-        about_dialog = Gtk.AboutDialog(transient_for=self.window, modal=True)
-        about_dialog.present()
+        about_dialog = Gtk.AboutDialog(transient_for=self.main_win, modal=True)
+        about_dialog.show()
 
-    def on_quit(self, action, param):
-        self.quit()
+    def do_shutdown(self):
+        # remove gui log handler
+        for h in lg.handlers:
+            if h.get_name() == 'ui':
+                lg.removeHandler(h)
+                lg.debug("Shutting down")
+        Gtk.Application.do_shutdown(self)
+
+    def on_runCodeButton_clicked(self, button):
+        lg.debug("Hello World!")
+
+    def on_iv_devs_icon_release(self, icon, a, b):
+        lg.debug("devs icon released")
+        dd = self.b.get_object("devDialog")
+        dd.present()
+
 
 if __name__ == "__main__":
-    app = Application()
+    app = App()
     app.run(sys.argv)
