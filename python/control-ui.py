0f211e976444b8788d21643e974294ea80d04942
jmball <jmball@users.noreply.github.com>
2020-06-14 15:42:56 +0100

    added run callback to send info via mqtt

--- python/control-ui.py
+++ python/control-ui.py
@@ -1,72 +1,92 @@
 #!/usr/bin/env python
 
 import pathlib
-#import time
-#import signal # to handle key kill
+
+# import time
+# import signal # to handle key kill
 import logging
 import systemd.journal
-#import logging.handlers
+
+# import logging.handlers
 import gi
 import sys
 import time
 import math
 import humanize
 import datetime as dt
-#os.environ["DEBUSSY"] = "1"
+import paho.mqtt.client as mqtt
+import configparser
+import json
 
-#gi.require_version('WebKit2', '4.0')
-gi.require_version('Gtk', '3.0')
-gi.require_version('Gdk', '3.0')
+# os.environ["DEBUSSY"] = "1"
+
+# gi.require_version('WebKit2', '4.0')
+gi.require_version("Gtk", "3.0")
+gi.require_version("Gdk", "3.0")
 from gi.repository import GLib, Gio, Gtk, Gdk, Pango
-#Gdk.set_allowed_backends('broadway')
-#from gi.repository.WebKit2 import WebView, Settings
-#gi.require_version('WebKit2', '4.0')
-#from gi.repository import WebKit2 as Webkit
+
+# Gdk.set_allowed_backends('broadway')
+# from gi.repository.WebKit2 import WebView, Settings
+# gi.require_version('WebKit2', '4.0')
+# from gi.repository import WebKit2 as Webkit
 
 
 # setup logging
-lg = logging.getLogger('control-ui')
+lg = logging.getLogger("control-ui")
 lg.setLevel(logging.DEBUG)
 sysL = systemd.journal.JournalHandler(SYSLOG_IDENTIFIER=lg.name)
 ch = logging.StreamHandler()
-logFormat = logging.Formatter(('%(asctime)s|%(name)s|%(levelname)s|%(message)s'))
-uiLogFormat = logging.Formatter(('%(asctime)s|%(levelname)s|%(message)s'))
-sysLogFormat = logging.Formatter(('%(levelname)s|%(message)s'))
+logFormat = logging.Formatter(("%(asctime)s|%(name)s|%(levelname)s|%(message)s"))
+uiLogFormat = logging.Formatter(("%(asctime)s|%(levelname)s|%(message)s"))
+sysLogFormat = logging.Formatter(("%(levelname)s|%(message)s"))
 sysL.setFormatter(sysLogFormat)
 ch.setFormatter(logFormat)
 lg.addHandler(ch)
 lg.addHandler(sysL)
 
-#Webkit.WebView()
-
-
+# Webkit.WebView()
 
 
 class App(Gtk.Application):
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, application_id="net.christoforo.control-ui",
-                         flags=Gio.ApplicationFlags.HANDLES_COMMAND_LINE,
-                         **kwargs)
+    def __init__(self, MQTTHOST="172.0.0.1", config_file="config.ini", *args, **kwargs):
+        super().__init__(
+            *args,
+            application_id="net.christoforo.control-ui",
+            flags=Gio.ApplicationFlags.HANDLES_COMMAND_LINE,
+            **kwargs,
+        )
         self.main_win = None
         self.b = None
         self.numPix = 6
         self.approx_seconds_per_iv = 50
-        self.substrate_designators = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
-        #self.substrate_designators = ['A1', 'A2', 'A3', 'A4', 'A5', 'B1', 'B2', 'B3', 'B4', 'B5', 'C1', 'C2', 'C3', 'C4', 'C5', 'D1', 'D2', 'D3', 'D4', 'D5']
+        self.substrate_designators = ["A", "B", "C", "D", "E", "F", "G", "H"]
+        # self.substrate_designators = ['A1', 'A2', 'A3', 'A4', 'A5', 'B1', 'B2', 'B3', 'B4', 'B5', 'C1', 'C2', 'C3', 'C4', 'C5', 'D1', 'D2', 'D3', 'D4', 'D5']
         self.numSubstrates = len(self.substrate_designators)
 
         galde_ui_xml_file_name = "ui.glade"
         gui_file = pathlib.Path(galde_ui_xml_file_name)
         if gui_file.is_file():
             self.galde_ui_xml_file = str(gui_file)
-        elif ("python"/gui_file).is_file():  # for debugging
-            self.galde_ui_xml_file = str("python"/gui_file)
+        elif ("python" / gui_file).is_file():  # for debugging
+            self.galde_ui_xml_file = str("python" / gui_file)
         else:
-            raise(ValueError("Can't find glade file!"))
+            raise (ValueError("Can't find glade file!"))
 
-        self.add_main_option("test", ord("t"), GLib.OptionFlags.NONE,
-                             GLib.OptionArg.NONE, "Command line test", None)
+        self.add_main_option(
+            "test",
+            ord("t"),
+            GLib.OptionFlags.NONE,
+            GLib.OptionArg.NONE,
+            "Command line test",
+            None,
+        )
+
+        # connect to mqtt broker
+        self.mqttc = mqtt.Client()
+        self.mqttc.connect(MQTTHOST)
+        self.mqttc.loop_start()
+
+        self.config_file = config_file
 
     def do_startup(self):
         lg.debug("Starting up app")
@@ -78,13 +98,13 @@ class App(Gtk.Application):
         self.label_tree, self.label_store = self.setup_label_tree()
         self.dev_tree, self.dev_store = self.setup_picker_tree()
 
-        max_devices = self.numPix*self.numSubstrates
-        address_string_length = math.ceil(max_devices/4)
+        max_devices = self.numPix * self.numSubstrates
+        address_string_length = math.ceil(max_devices / 4)
         selection_box_length = 4 + address_string_length + 3
-        default_on = '0x'+'F'*address_string_length
-        default_off = '0x'+'0'*address_string_length
-        self.iv_dev_box = self.b.get_object('iv_devs')
-        self.eqe_dev_box = self.b.get_object('eqe_devs')
+        default_on = "0x" + "F" * address_string_length
+        default_off = "0x" + "0" * address_string_length
+        self.iv_dev_box = self.b.get_object("iv_devs")
+        self.eqe_dev_box = self.b.get_object("eqe_devs")
         self.def_fmt_str = f"0{address_string_length}X"
 
         # TODO: do this in a non-obsolete way (probably w/ css)
@@ -94,12 +114,12 @@ class App(Gtk.Application):
         self.iv_dev_box.set_text(default_on)
         self.last_valid_devs = default_on
         self.iv_dev_box.set_width_chars(selection_box_length)
-        self.iv_dev_box.set_icon_from_icon_name(0, 'emblem-default')
+        self.iv_dev_box.set_icon_from_icon_name(0, "emblem-default")
         self.eqe_dev_box.modify_font(fontdesc)
         self.eqe_dev_box.set_text(default_off)
         self.eqe_dev_box.set_width_chars(selection_box_length)
 
-        self.po = self.b.get_object('iv_pop')
+        self.po = self.b.get_object("iv_pop")
         self.po.set_position(Gtk.PositionType.BOTTOM)
         self.tick()
         self.ticker = self.timeout_id = GLib.timeout_add_seconds(1, self.tick, None)
@@ -112,7 +132,7 @@ class App(Gtk.Application):
         inconsistent = False
         # fill in the model
         for i in range(self.numSubstrates):
-            if self.label_store[i][0] == '':
+            if self.label_store[i][0] == "":
                 label = self.label_store[i][1]
             else:
                 label = self.label_store[i][0]
@@ -143,7 +163,7 @@ class App(Gtk.Application):
 
     # callback function for select/deselect device/substrate
     def dev_toggle(self, widget, path):
-        path_split = path.split(':')
+        path_split = path.split(":")
         # the boolean value of the selected row
         current_value = self.dev_store[path][1]
         # change the boolean value of the selected row in the model
@@ -186,7 +206,7 @@ class App(Gtk.Application):
                 self.dev_store[piter][2] = True
 
         # iterate through everything and build up the result
-        siter = self.dev_store.get_iter('0')  # substrate iterator
+        siter = self.dev_store.get_iter("0")  # substrate iterator
         selection_bitmask = 0
         bit_location = 0
         while siter is not None:
@@ -206,11 +226,11 @@ class App(Gtk.Application):
         if len(text_is) == len(f"0x{0:{self.def_fmt_str}}"):
             try:
                 is_upper = text_is.upper()
-                if is_upper[0:2] == '0X':
+                if is_upper[0:2] == "0X":
                     num_part = is_upper[2::]
-                    valid_chars = '0123456789ABCDEF'
+                    valid_chars = "0123456789ABCDEF"
                     filtered = filter(lambda ch: ch in valid_chars, num_part)
-                    num_part = ''.join(filtered)
+                    num_part = "".join(filtered)
                     selection_bitmask = int(num_part, 16)
                     should_be = f"0x{selection_bitmask:{self.def_fmt_str}}"
                     if text_is == should_be:
@@ -220,13 +240,13 @@ class App(Gtk.Application):
 
         if valid is True:
             self.last_valid_devs = text_is
-            self.iv_dev_box.set_icon_from_icon_name(0, 'emblem-default')
+            self.iv_dev_box.set_icon_from_icon_name(0, "emblem-default")
             self.iv_measure_note(selection_bitmask)
         else:
-            self.iv_dev_box.set_icon_from_icon_name(0, 'dialog-error')
+            self.iv_dev_box.set_icon_from_icon_name(0, "dialog-error")
 
     def on_iv_devs_focus_out_event(self, event, user_data=None):
-        if self.iv_dev_box.get_icon_name(0) != 'emblem-default':
+        if self.iv_dev_box.get_icon_name(0) != "emblem-default":
             self.iv_dev_box.set_text(self.last_valid_devs)
         else:
             self.iv_dev_box.set_text(self.last_valid_devs)
@@ -243,9 +263,11 @@ class App(Gtk.Application):
 
     # log message printer for device selection change
     def iv_measure_note(self, selection_bitmask):
-        num_selected = sum([c == '1' for c in bin(selection_bitmask)])
+        num_selected = sum([c == "1" for c in bin(selection_bitmask)])
         lg.info(f"{num_selected} devices selected for I-V measurement")
-        duration_string = humanize.naturaldelta(dt.timedelta(seconds=self.approx_seconds_per_iv*num_selected))
+        duration_string = humanize.naturaldelta(
+            dt.timedelta(seconds=self.approx_seconds_per_iv * num_selected)
+        )
         lg.info(f"Which might take {duration_string} to measure")
 
     def setup_label_tree(self):
@@ -255,19 +277,21 @@ class App(Gtk.Application):
         label = ""
         for i in range(self.numSubstrates):
             # the iter piter is returned when appending the author
-            #designator = self.substrate_designators[i]
+            # designator = self.substrate_designators[i]
             labelStore.append([label, self.substrate_designators[i], y_pad])
         labelTree.set_model(labelStore)
 
         # the uneditable substrate designator col
-        #renderText = Gtk.CellRendererText()
-        #numbers = Gtk.TreeViewColumn("Substrate", renderText, text=0, ypad=3)
-        #labelTree.append_column(numbers)
+        # renderText = Gtk.CellRendererText()
+        # numbers = Gtk.TreeViewColumn("Substrate", renderText, text=0, ypad=3)
+        # labelTree.append_column(numbers)
 
         # the editable substrate label col
         renderEdit = Gtk.CellRendererText()
         renderEdit.set_property("editable", True)
-        labels = Gtk.TreeViewColumn("Substrate Label", renderEdit, text=0, placeholder_text=1, ypad=2)
+        labels = Gtk.TreeViewColumn(
+            "Substrate Label", renderEdit, text=0, placeholder_text=1, ypad=2
+        )
         labelTree.append_column(labels)
 
         renderEdit.connect("edited", self.store_substrate_label)
@@ -278,15 +302,17 @@ class App(Gtk.Application):
     # handles keystroke in the label creation tree
     def handle_label_key(self, tv, event):
         keyname = Gdk.keyval_name(event.keyval)
-        if keyname in ['Return', 'Enter']:
+        if keyname in ["Return", "Enter"]:
             path, col = self.label_tree.get_cursor()
             path.next()
-            self.label_tree.set_cursor_on_cell(path, focus_column=col, focus_cell=None, start_editing=True)
+            self.label_tree.set_cursor_on_cell(
+                path, focus_column=col, focus_cell=None, start_editing=True
+            )
 
     # handles keystroke in the device selection tree
     def handle_dev_key(self, tv, event):
         keyname = Gdk.keyval_name(event.keyval)
-        if keyname in ['Right', 'Left']:
+        if keyname in ["Right", "Left"]:
             path, col = self.dev_tree.get_cursor()
             if self.dev_tree.row_expanded(path) is True:
                 self.dev_tree.collapse_row(path)
@@ -295,7 +321,7 @@ class App(Gtk.Application):
 
     def store_substrate_label(self, widget, path, text):
         self.label_store[path][0] = text
-        if text == '':
+        if text == "":
             self.dev_store[path][0] = self.label_store[path][1]
         else:
             self.dev_store[path][0] = self.label_store[path][0]
@@ -330,7 +356,7 @@ class App(Gtk.Application):
         rnp = self.b.get_object("run_name_prefix")
         rn = self.b.get_object("run_name")
         rns = self.b.get_object("run_name_suffix")
-        rn.set_text(rnp.get_text()+rns.get_text())
+        rn.set_text(rnp.get_text() + rns.get_text())
 
     def do_activate(self):
         lg.debug("Activating app")
@@ -352,7 +378,7 @@ class App(Gtk.Application):
             self.logTB.write = myWrite
             self.logTB.flush = myFlush
             uiLog = logging.StreamHandler(stream=self.logTB)
-            uiLog.set_name('ui')
+            uiLog.set_name("ui")
             uiLog.setFormatter(uiLogFormat)
             lg.addHandler(uiLog)
             lg.debug("Gui logging setup.")
@@ -382,7 +408,7 @@ class App(Gtk.Application):
     def do_shutdown(self):
         # remove gui log handler
         for h in lg.handlers:
-            if h.get_name() == 'ui':
+            if h.get_name() == "ui":
                 lg.removeHandler(h)
                 lg.debug("Shutting down")
         Gtk.Application.do_shutdown(self)
@@ -392,8 +418,8 @@ class App(Gtk.Application):
 
     def on_iv_devs_icon_release(self, icon, b, user_data=None):
         sw = self.dev_tree.get_parent()  # scroll window
-        sw.set_min_content_height((self.numSubstrates+1)*25)
-        if self.iv_dev_box.get_icon_name(0) != 'emblem-default':
+        sw.set_min_content_height((self.numSubstrates + 1) * 25)
+        if self.iv_dev_box.get_icon_name(0) != "emblem-default":
             self.iv_dev_box.set_text(self.last_valid_devs)
         text_is = self.iv_dev_box.get_text()
         selection_bitmask = int(text_is, 16)
@@ -402,13 +428,17 @@ class App(Gtk.Application):
         bin_mask_rev = bin_mask[::-1]
 
         # iterate through everything and build up the result
-        siter = self.dev_store.get_iter('0')  # substrate iterator
+        siter = self.dev_store.get_iter("0")  # substrate iterator
         bit_location = 0
         while siter is not None:
-            num_enabled = 0  # keeps track of number of enabled devices on this substrate
+            num_enabled = (
+                0  # keeps track of number of enabled devices on this substrate
+            )
             diter = self.dev_store.iter_children(siter)  # device iterator
             while diter is not None:
-                if (bit_location + 1 <= len(bin_mask_rev)) and (bin_mask_rev[bit_location] == '1'):
+                if (bit_location + 1 <= len(bin_mask_rev)) and (
+                    bin_mask_rev[bit_location] == "1"
+                ):
                     self.dev_store[diter][1] = True
                     num_enabled = num_enabled + 1
                 else:
@@ -422,12 +452,32 @@ class App(Gtk.Application):
             else:
                 self.dev_store[siter][2] = True  # set substrate inconsistant
             siter = self.dev_store.iter_next(siter)  # advance to the next substrate
-            
-        
 
         self.po.show_all()
-        #lg.debug(sw.get_allocated_height())
+        # lg.debug(sw.get_allocated_height())
         return True
+
+    def start_run(self):
+        """Send run info to experiment orchestrator via MQTT."""
+        # load info from config file
+        config = configparser.ConfigParser()
+        config.read(self.config_file)
+
+        save_folder = pathlib.Path(config["paths"]["save_folder"])
+        run_name = self.b.get_object("run_name")
+        destination = str(save_folder.joinpath(run_name))
+
+        # make settings dict
+        settings = {
+            "destination": destination,
+            "operator": "",
+            "run_description": "",
+            "experimental_parameter": "",
+        }
+
+        # send settings dict over mqtt
+        payload = json.dumps(settings)
+        self.mqttc.publish("gui", payload, qos=2).wait_for_publish()
 
 
 if __name__ == "__main__":
