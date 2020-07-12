d0be45f0daa32fcd5daee7a529528a8f306208fb
Greyson Christoforo <grey@christoforo.net>
2020-07-12 12:39:14 +0100

    add dep

--- get_deps.sh
+++ get_deps.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo pacman -Syu --needed python-systemd python-humanize python-paho-mqtt python-gobject
+sudo pacman -Syu --needed python-systemd python-humanize python-paho-mqtt python-gobject webkit2gtk
