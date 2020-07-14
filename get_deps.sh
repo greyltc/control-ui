1edf880bedf040aa8bfec8d3e9e89569bb99239d
Greyson Christoforo <grey@christoforo.net>
2020-07-14 12:36:53 +0100

    add print only deps option

--- get_deps.sh
+++ get_deps.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/env bash
-sudo pacman -Syu --needed python-systemd python-humanize python-paho-mqtt python-gobject webkit2gtk
+
+ARG1=${1:-no-print}
+
+PACMAN_DEPS="python-systemd python-humanize python-paho-mqtt python-gobject webkit2gtk"
+
+if test "${ARG1}" = "print"
+then
+	echo "${PACMAN_DEPS}"
+else
+	sudo pacman -Syu --needed ${PACMAN_DEPS}
+fi
